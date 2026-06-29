88056c95848ded6c0fcd1a4f9e7e06804a8811a9
yashashvialva <alvayashashvi@gmail.com>
2026-06-30 00:56:33 +0530

    Fix Vercel Prisma Query Engine not found by externalizing packages in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', '@prisma/customer-client'],
+  },
 };
 
 export default nextConfig;
